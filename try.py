8a4d25f48fb8725e452dc9ebf1ea9d8faf4a0808
weixiang <weixiang0124@gmail.com>
2025-06-18 00:01:25 +0800

    done

--- try.py
+++ try.py
@@ -1,28 +1,60 @@
 import requests
 from PIL import Image
+from bs4 import BeautifulSoup
+import os
 
-# Image URL
-url = "https://i.pinimg.com/236x/ba/c7/12/bac7127211c77188ea44b7e5042425ad.jpg"
+
+
 
 # Send HTTP GET request
-response = requests.get(url)
+def getImageUrl(url):
+    imageUrls = []
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Save the image to a file
-    with open("downloaded_image.jpg", "wb") as f:
-        f.write(response.content)
-    print("Image downloaded successfully.")
-else:
-    print(f"Failed to download image. Status code: {response.status_code}")
+    response = requests.get(pinterest_board_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        imgs = soup.find_all("img")
+
+        for img in imgs:
+            src = img.get("src")
+            if src and "pinimg.com" and "236x" in src:
+                imageUrls.append(src)
+
+    return imageUrls
+
+def downloadImages(imageUrls):
+    imagesPath = []
+    imageName = 1
+    for url in imageUrls:
+        response = requests.get(url)
+        imagePath = f"./images/{imageName}.jpg"
+        imagesPath.append(imagePath)
+        imageName += 1
+        with open(imagePath, "wb") as f:
+            f.write(response.content)
+    return imagesPath
+
+def convertToJPEG(imagesPath):
+    for imagePath in imagesPath:
+        if (imagePath.endswith(".jpg")):
+            jpegPath = imagePath.split(".jpg")[0]
+            image = Image.open(imagePath)
+            image.save(f"{jpegPath}.jpeg", "JPEG")
+            os.remove(imagePath)
 
 
 
+# Pinterest board URL
+pinterest_board_url = "https://www.pinterest.com/jaycheng120/test/"
 
-# Open the .jpg image
-image = Image.open("downloaded_image.jpg")
+imageUrls = getImageUrl(pinterest_board_url)
 
-# Save the image as .jpeg
-image.save("image_converted.jpeg", "JPEG")
+imagesPath = downloadImages(imageUrls)
+
+convertToJPEG(imagesPath)
+
+
 
 
