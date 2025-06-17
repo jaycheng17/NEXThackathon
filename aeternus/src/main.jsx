ad51003ef873fca775ff25d5c467d5ac787bbdb1
Jeryl Khoo <12jerylkhoo@gmail.com>
2025-06-18 04:12:09 +0800

    Log in Log out done

--- aeternus/src/main.jsx
+++ aeternus/src/main.jsx
@@ -2,10 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')).render(
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_4rVTRTB91",
+  client_id: "2edgpr2d91i7rds9hejqif6nkp",
+  redirect_uri: "http://localhost:5173/services/",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
+const root = createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
--- aeternus/src/pages/Services.jsx
+++ aeternus/src/pages/Services.jsx
@@ -1,5 +1,42 @@
-const Services = () => {
-  return <h1>Services</h1>;
-};
+import { useAuth } from "react-oidc-context";
+
+function Services() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "2edgpr2d91i7rds9hejqif6nkp";
+    const logoutUri = "http://localhost:5173/";
+    const cognitoDomain = "https://us-west-24rvtrtb91.auth.us-west-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
+}
 
 export default Services;
