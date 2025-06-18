1fcbff6ff19f7d8ee46a56b58ed799ca42aa6044
Jeryl Khoo <12jerylkhoo@gmail.com>
2025-06-18 22:43:51 +0800

    left gallery text and image gen

--- aeternus/src/pages/Gallery.jsx
+++ aeternus/src/pages/Gallery.jsx
@@ -7,7 +7,7 @@ const Gallery = () => {
       <div className="cardDiv" align="center" >
         <Card name="Wei Xiang" test="I LOVE AWS LAMBDA" imgs="/src/img/wx.jpg" />
         <Card name="Matthew" test="" imgs="/src/img/matt.jpg"/>
-        <Card name="Jeryl" test="" imgs="/src/img/jeryl.png"/>
+        <Card name="Jeryl" test="You want a Bride?&#13;I'll give you a Bribe :D" imgs="/src/img/jeryl.png"/>
         <Card name="Jay" test="This app helped me get married in 2 days" imgs="/src/img/jay.png"/>
       </div>  
 
