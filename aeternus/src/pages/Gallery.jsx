790eee6acee0da0cb5836cd572204e9c12efed71
weixiang <weixiang0124@gmail.com>
2025-06-18 20:58:16 +0800

    Add Gallery component with card layout and styles

--- aeternus/src/pages/Gallery.jsx
+++ aeternus/src/pages/Gallery.jsx
@@ -1,5 +1,18 @@
+import Card from '../Card/Card.jsx'
+import "../css/Gallery.css"
+
 const Gallery = () => {
-  return <h1>Gallery</h1>;
+  return (
+
+      <div className="cardDiv" align="center" >
+        <Card name="Wei Xiang" test="I LOVE AWS LAMBDA" imgs="/src/img/wx.jpg" />
+        <Card name="Matthew" test="" imgs="/src/img/matt.jpg"/>
+        <Card name="Jeryl" test="" imgs="/src/img/jeryl.png"/>
+        <Card name="Jay" test="This app helped me get married in 2 days" imgs="/src/img/jay.png"/>
+      </div>  
+
+
+  );
 };
 
 export default Gallery;
