aa667ac4a5c69b5dc519a3ad561559a5fdb5f7aa
Jeryl Khoo <12jerylkhoo@gmail.com>
2025-06-19 04:12:53 +0800

    missing images fixed

--- aeternus/src/pages/Gallery.jsx
+++ aeternus/src/pages/Gallery.jsx
@@ -1,14 +1,17 @@
 import Card from '../Card/Card.jsx'
 import "../css/Gallery.css"
+import wx from "../img/wx.jpg";
+import jk from "../img/jeryl.png";
+import matt from "../img/matt.jpg";
+import jay from "../img/jay.png";
 
 const Gallery = () => {
   return (
-
       <div className="cardDiv" align="center" >
-        <Card name="Wei Xiang" test="I LOVE AWS LAMBDA" imgs="/src/img/wx.jpg" />
-        <Card name="Matthew" test="" imgs="/src/img/matt.jpg"/>
-        <Card name="Jeryl" test="You want a Bride?&#13;I'll give you a Bribe :D" imgs="/src/img/jeryl.png"/>
-        <Card name="Jay" test="This app helped me get married in 2 days" imgs="/src/img/jay.png"/>
+        <Card name="Wei Xiang" test="I LOVE AWS LAMBDA" imgs={wx} />
+        <Card name="Matthew" test="sentence_transformers LOL" imgs={matt}/>
+        <Card name="Jeryl" test="You want a Bride? I'll give you a Bribe :D" imgs={jk}/>
+        <Card name="Jay" test="This app helped me get married in 2 days" imgs={jay}/>
       </div>  
 
 
--- aeternus/src/pages/Home.jsx
+++ aeternus/src/pages/Home.jsx
@@ -3,16 +3,6 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
   return (
-    // <Container fluid className="d-flex justify-content-center align-items-center vh-100 px-0">
-    //   <div className="text-center">
-    //     <Row>
-    //       <Col className="Head">Forever & Always</Col>
-    //     </Row>
-    //     <Row>
-    //       <Col className="Desc">Bespoke Wedding Styling & Planning</Col>
-    //     </Row>
-    //   </div>
-    // </Container>
     <div className="Homediv">
       <div className="home-content">
         <p className="Head">Forever & Always</p>
